9c4b29175436f9000468ac020b08add23a3d3a17
unknown <eugenz2000@gmail.com>
2019-11-17 18:42:00 +0800

    added header file to threadview

---
+++ threadview.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="./resources/css/index.css" type="text/css" rel ="stylesheet">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>AgriTalk</title>
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+	<?php include "header.php";?>
+	<?php include "cropinfo.php";?>
+
+
+
+
+
+
+
+</body>
+</html>
