ce2ad5fa60ddfa76b461d26edd3c14fd6d915f1d
hcyko1 <hcyko1@nottingham.edu.my>
2020-02-23 18:08:47 +0800

    uploading image in threads feature

--- threadcreate.php
+++ threadcreate.php
@@ -19,7 +19,7 @@
 	</header>
 	
 	<div id = "threadcreate">
-		<form method="post">
+		<form method="post" enctype="multipart/form-data">
 		  <div>
 		  	<label>TITLE:</label>
 		  </div>
@@ -39,7 +39,6 @@
 
 <?php
 if(isset($_POST['SUBMIT'])){
-	
 	//Connection details
 	$servername = "localhost";
 	$dbUsername 	= "hcyko1";
@@ -65,15 +64,22 @@ if(isset($_POST['SUBMIT'])){
 	$title = $_POST['TITLE'];
 	$content = $_POST['CONTENT'];
 	
-	//Deal with images later
-	/*
-	// image uploading
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-	$uploadOk = 1;
-	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	// Check if image file is a actual image or fake image
-	if(isset($_GET["submit"])) {
+	//generate current time
+	$result = mysqli_query($conn, "SELECT CURRENT_TIMESTAMP()");
+	$time = mysqli_fetch_assoc($result)['CURRENT_TIMESTAMP()'];
+	
+	// images upload
+	if($_FILES["fileToUpload"]["name"] == ''){ //no file uploaded
+		$fileSelected = 0;
+		$imageDone = 0;
+	}
+	else{
+		$fileSelected = 1;
+		$imageDone = 0;
+		$uploadOk = 1;;
+		$imageFileType = strtolower(pathinfo($_FILES["fileToUpload"]["name"],PATHINFO_EXTENSION));
+		
+		// Check if image file is a actual image or fake image
 		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 		if($check !== false) {
 			echo "File is an image - " . $check["mime"] . ".";
@@ -82,26 +88,55 @@ if(isset($_POST['SUBMIT'])){
 			echo "File is not an image.";
 			$uploadOk = 0;
 		}
-	}
-	*/
-	
-	if(strlen($title) > 0 && strlen($content) > 0){
-		
-		//generate current time
-		$result = mysqli_query($conn, "SELECT CURRENT_TIMESTAMP()");
-		$time = mysqli_fetch_assoc($result)['CURRENT_TIMESTAMP()'];
-		
-		$AddQuery = "INSERT INTO post (userID, title, text, postTime)
-					 VALUES ('$userID', '$title', '$content', '$time')";
-		
-		if (mysqli_query($conn, $AddQuery)) {
-			$sql = "SELECT postID FROM post WHERE userID='$userID' AND postTime='$time'";
-			$result = mysqli_query($conn, $sql);
-
-			$postID = mysqli_fetch_assoc($result)['postID'];
-			echo '<meta http-equiv="Refresh" content="0; url=threadview.php?postID=' . $postID . '" />';
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+			echo "Sorry, your file is too large.";
+			$uploadOk = 0;
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+		&& $imageFileType != "gif" ) {
+			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			$uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			echo "Sorry, your file was not uploaded.";
+		// if everything is ok, try to upload file
 		} else {
-			echo "Error: " . $AddQuery . "<br>" . mysqli_error($conn);
+			//create record in image table
+			$AddQuery = "INSERT INTO image (format, userID, uploadTime)
+						 VALUES ('$imageFileType', '$userID', '$time')";
+			if(!mysqli_query($conn, $AddQuery)){
+				$imageDone = 0;
+			}
+			$sql = "SELECT imageID FROM image WHERE userID='$userID' AND uploadTime='$time'";
+			$result = mysqli_query($conn, $sql);
+			$imageID = mysqli_fetch_assoc($result)['imageID'];
+			
+			//image storage location
+			$target_dir = "uploads/";
+			$target_file = $target_dir . $imageID . '.' . $imageFileType;
+			
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				$imageDone = 1;
+				echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+			} else {
+				echo "Sorry, there was an error uploading your file.";
+			}
+		}
+	}
+	
+	//text upload
+	$textDone = 0;
+	if(strlen($title) > 0 && strlen($content) > 0){
+		if($imageDone == 0){
+			$AddQuery = "INSERT INTO post (userID, title, text, imageID, postTime)
+						 VALUES ('$userID', '$title', '$content', NULL, '$time')";
+		}
+		else{
+			$AddQuery = "INSERT INTO post (userID, title, text, imageID, postTime)
+						 VALUES ('$userID', '$title', '$content', '$imageID', '$time')";
 		}
 	}
 	else{
@@ -111,6 +146,33 @@ if(isset($_POST['SUBMIT'])){
 		</script>
 		';
 	}
+	
+	if (mysqli_query($conn, $AddQuery)) {
+		$textDone = 1;
+		$sql = "SELECT postID FROM post WHERE userID='$userID' AND postTime='$time'";
+		$result = mysqli_query($conn, $sql);
+		$postID = mysqli_fetch_assoc($result)['postID'];
+	} else {
+		echo "Error: " . $AddQuery . "<br>" . mysqli_error($conn);
+	}
+	
+	
+	
+	if($textDone == 1 && ($fileSelected == 0 || $imageDone == 1)){ //go to thread created
+		echo '<meta http-equiv="Refresh" content="0; url=threadview.php?postID=' . $postID . '" />';
+	}
+	else if($textDone == 0 && $imageDone == 1){ //text upload failed
+		unlink($target_file); //delete image
+	}
+	else if($textDone == 1 && $fileSelected == 1 && $imageDone == 0){ //image upload failed
+		if($uploadOk == 1){
+			$DeleteQuery = "DELETE FROM image WHERE imageID=" . $imageID; //delete image record from db
+			mysqli_query($conn, $DeleteQuery);
+		}
+		$DeleteQuery = "DELETE FROM post WHERE postID=" . $postID; //delete post from db
+		mysqli_query($conn, $DeleteQuery);
+	}
+	
 	mysqli_close($conn);
 }	
 ?>
