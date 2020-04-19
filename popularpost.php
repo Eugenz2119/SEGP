b3374d0c395622afb8b82837ecabcc013d554496
hcyko1 <hcyko1@nottingham.edu.my>
2020-04-19 18:09:05 +0800

    integrated popular posts functionality

--- popularpost.php
+++ popularpost.php
@@ -10,30 +10,54 @@
 		<div>
 			<h3>Popular Posts</h3>
 		</div>
+<?php
+$postLim = 5;
 
+//Connection details
+$servername = "localhost";
+$dbUsername 	= "hcyko1_admin";
+$dbPassword 	= "3QXBfTmKAccZ0BNO";
+$dbname 	= "hcyko1_agritalk";
+
+// Create connection
+$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbname);
+// Check connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+//debug
+else{
+	//echo "DB CONNECTED";
+}
+
+//generate current time
+$result = mysqli_query($conn, "SELECT CURRENT_TIMESTAMP()");
+$time = mysqli_fetch_assoc($result)['CURRENT_TIMESTAMP()'];
+
+$sql ="SELECT * FROM post WHERE (SELECT DATEDIFF('$time', lastCommentTime))<7 ORDER BY commentCount DESC, lastCommentTime DESC LIMIT " . $postLim;
+$result=mysqli_query($conn,$sql);
+
+while($row = mysqli_fetch_assoc($result)) {
+	$postID = $row['postID'];
+	$cropID = $row['cropID'];
+	$query = "SELECT cropname FROM crop WHERE cropID=" . $cropID;
+	$cropname = mysqli_fetch_assoc(mysqli_query($conn, $query))['cropname'];
+	$title = $row['title'];
+	$posterID = $row['userID'];
+	$query = "SELECT username FROM user WHERE userID=" . $posterID;
+	$postUsername = mysqli_fetch_assoc(mysqli_query($conn, $query))['username'];
+	echo '
 		<div class="poppreview">
-			<a>in : <a href ="cropsumforum.php">PLACEHOLDER CROP NAME</a>
-			<h5><a href="threadview.php">TITLE</a></h5>
+			<a>in : <a href ="cropsubforum.php?cropID=' . $cropID . '">' . $cropname . '</a>
+			<h5><a href="threadview.php?postID=' . $postID . '">' . $title . '</a></h5>
 			<div class="postuser">
-				<a>by : <a href ="userprofile.php">USERNAME</a></a>
+				<a>by : <a href ="userprofile.php?userID=' . $posterID . '">' . $postUsername . '</a></a>
 			</div>
 		</div>
+	';
+}
 
-		<div class="poppreview">
-			<a>in : <a href ="cropsumforum.php">PLACEHOLDER CROP NAME</a>
-			<h5><a href="threadview.php">TITLE</a></h5>
-			<div class="postuser">
-				<a>by : <a href ="userprofile.php">USERNAME</a></a>
-			</div>
-		</div>
-
-		<div class="poppreview">
-			<a>in : <a href ="cropsumforum.php">PLACEHOLDER CROP NAME</a>
-			<h5><a href="threadview.php">TITLE</a></h5>
-			<div class="postuser">
-				<a>by : <a href ="userprofile.php">USERNAME</a></a>
-			</div>
-		</div>
+?>
 	</div>
 
 
