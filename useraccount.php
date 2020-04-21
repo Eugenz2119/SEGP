498839b485d29153bbcb2fce70658a33b4e302f7
hcyko1 <hcyko1@nottingham.edu.my>
2020-04-22 00:12:42 +0800

    personal details changing feature

--- useraccount.php
+++ useraccount.php
@@ -13,9 +13,9 @@
 	<?php
 	//Connection details
 	$servername = "localhost";
-	$dbUsername 	= "hcyko1";
+	$dbUsername 	= "hcyko1_admin";
 	$dbPassword 	= "3QXBfTmKAccZ0BNO";
-	$dbname 	= "agritalk-wip";
+	$dbname 	= "hcyko1_agritalk";
 
 	// Create connection
 	$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbname);
@@ -36,6 +36,16 @@
 		echo '<meta http-equiv="Refresh" content="0; url=login.php" />';
 	}
 	
+	$sql = "SELECT * FROM user WHERE userID=" . $userID;
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	$Age = $row['age'];
+	$Email = $row['email'];
+	$Institution = $row['institution'];
+	$Occupation = $row['occupation'];
+	$Country = $row['country'];
+	$PhoneNumber = $row['phonenum'];
+	
 	?>
 
 	<div class = "settings">
@@ -45,6 +55,10 @@
 			<div id="general" onclick="generalsetting()"style="background-color: #E1E1E1">
 				<h4 class="gen">General</h4>
 			</div>
+			
+			<div id="password" onclick="passwordsetting()">
+				<h4 class="pass">Password</h4>
+			</div>
 
 			<div id="privacy" onclick="privacysetting()">
 				<h4 class="priv">Privacy</h4>
@@ -53,22 +67,41 @@
 		</div>
 
 		<div class = "specifics">
+<?php
+echo '
 			<div id = "generalsetting">
-				<p style="padding-left: 10px; font-size: 18px;">
-				Change Password <br/>
-				Old Password 	: <input type="password" name="oldpw"><br/>
-				New Password 	: <input type="password" name="newpw"><br/><br/>
-				Change Profile Picture <br/>
-				<input type="file" name="profilepic" id="profilepic"><br/><br/>
-				Change Age(?) : <input type="integer" name="Age"><br/>
-				Change Email : <input type="text" name="Email"><br/>
-				Change Institution : <input type="text" name= "Institution"><br/>
-				Change Occupation : <input type="text" name= "Occupation"><br/>
-				Change Country : <input type="text" name= "Country"><br/>
-				Change Phone Number : <input type="integer" name= "PhoneNumber"><br/>
-				</p>
+				<form method="post">
+					<p style="padding-left: 10px; font-size: 18px;">
+					Change Profile Picture <br/>
+					<input type="file" name="profilepic" id="profilepic"><br/><br/>
+					
+					Change Age(?) : <input type="integer" name="Age" value="' . $Age . '"><br/>
+					Change Email : <input type="text" name="Email" value="' . $Email . '"><br/>
+					Change Institution : <input type="text" name= "Institution" value="' . $Institution . '"><br/>
+					Change Occupation : <input type="text" name= "Occupation" value="' . $Occupation . '"><br/>
+					Change Country : <input type="text" name= "Country" value="' . $Country . '"><br/>
+					Change Phone Number : <input type="integer" name= "PhoneNumber" value="' . $PhoneNumber . '"><br/>
+					</p>
+					
+					<input type="submit" class="savechanges" value="Save Changes" name="gensavebtn">
+					<input type="submit" class="cancelchanges" value="Cancel" name="cancelbtn">
+					
+				</form>
 			</div>
-
+			
+			<div id = "passwordsetting"  style="display: none;>
+				<form method="post">
+					<p style="padding-left: 10px; font-size: 18px;">
+					
+					Change Password <br/>
+					Old Password 	: <input type="password" name="oldpw"><br/>
+					New Password 	: <input type="password" name="newpw"><br/><br/>
+					</p>
+					
+					<input type="submit" class="savechanges" value="Save Changes" name="passsavebtn">
+					<input type="submit" class="cancelchanges" value="Cancel" name="cancelbtn">
+				</form>
+			</div>
 
 			<div id = "privacysetting" style="display: none;">
 				<p style ="font-size: 25px; padding-left:10px;">
@@ -77,12 +110,13 @@
 					Show Institution <label class="switch"><input type="checkbox" checked><span class="slider round"></span></label> <br/><br/>
 					Show Occupation <label class="switch"><input type="checkbox" checked><span class="slider round"></span></label> <br/><br/>
 					Show Country <label class="switch"><input type="checkbox" checked><span class="slider round"></span></label> <br/><br/>
-					Show Phone Number <label class="switch"><input type="checkbox" checked><span class="slider round"></span></label> </p>
+					Show Phone Number <label class="switch"><input type="checkbox" checked><span class="slider round"></span></label>
+				</p>
 			</div>
 		</div>
 
-		<a class="cancelchanges"href="#">Cancel</a>
-		<a class="savechanges"href="#">Save Changes</a>
+';
+?>
 
 	<!--profile picture
 	$sql="SELECT imageID FROM user WHERE userID =" . $userID;
@@ -112,21 +146,88 @@
 		<a href="#">change avatar</a>
 	</div>-->
 	
-</div>
-
+	</div>
+	
+<?php
+if(isset($_POST['gensavebtn'])){
+	$completeField = 0;
+	$Age = $_POST['Age'];
+	if(strlen($Age) != 0){
+		$completeField += 1;
+	}
+	else{
+		echo '
+		<script language="javascript">
+			alert("Age cannot be empty")
+		</script>
+		';
+	}
+	$Email = $_POST['Email'];
+	if(strlen($Email) != 0){
+		$completeField += 1;
+	}
+	else{
+		echo '
+		<script language="javascript">
+			alert("Email cannot be empty")
+		</script>
+		';
+	}
+	$Institution = $_POST['Institution'];
+	$Occupation = $_POST['Occupation'];
+	$Country = $_POST['Country'];
+	$PhoneNumber = $_POST['PhoneNumber'];
+	
+	if($completeField == 2){
+		$sql = "UPDATE user SET 
+		age=$Age, 
+		email='$Email', 
+		institution='$Institution', 
+		occupation='$Occupation', 
+		country='$Country', 
+		phonenum='$PhoneNumber' 
+		WHERE userID=$userID";
+		
+		if (mysqli_query($conn, $sql)) {
+			echo '
+			<script language="javascript">
+				alert("Account details updated successfully")
+			</script>
+			';
+			echo '<meta http-equiv="Refresh" content="0; url=useraccount.php" />';
+		}
+	}
+}
+if(isset($_POST['cancelbtn'])){
+	echo '<meta http-equiv="Refresh" content="0; url=useraccount.php" />';
+}
+?>
 <script type="text/javascript">
 function generalsetting(){
 	document.getElementById("generalsetting").style.display = "block";
+	document.getElementById("passwordsetting").style.display = "none";
 	document.getElementById("privacysetting").style.display = "none";
 	document.getElementById("general").style.backgroundColor = "#E1E1E1";
+	document.getElementById("password").style.backgroundColor = "white";
 	document.getElementById("privacy").style.backgroundColor = "white";
 }	
 
-function privacysetting(){
-	document.getElementById("privacysetting").style.display = "block" ;
+function passwordsetting(){
 	document.getElementById("generalsetting").style.display = "none"
-	document.getElementById("privacy").style.backgroundColor = "#E1E1E1";
+	document.getElementById("passwordsetting").style.display = "block";
+	document.getElementById("privacysetting").style.display = "none";
 	document.getElementById("general").style.backgroundColor = "white";
+	document.getElementById("password").style.backgroundColor = "#E1E1E1";
+	document.getElementById("privacy").style.backgroundColor = "white";
+}
+
+function privacysetting(){
+	document.getElementById("generalsetting").style.display = "none"
+	document.getElementById("passwordsetting").style.display = "none";
+	document.getElementById("privacysetting").style.display = "block";
+	document.getElementById("general").style.backgroundColor = "white";
+	document.getElementById("password").style.backgroundColor = "white";
+	document.getElementById("privacy").style.backgroundColor = "#E1E1E1";
 }
 
 
