ab94d054c2f522a22ae90e5a2048492796e51e46
hcyko1 <hcyko1@nottingham.edu.my>
2020-04-22 21:22:09 +0800

    page number buttons at top and bottom of content

--- homepage.php
+++ homepage.php
@@ -41,49 +41,19 @@ if(!isset($_GET['page'])){
 else{
 	$page = $_GET['page'];
 }
-$this_page_first_result = ($page-1)*$threadLim;
-
-$sql ='SELECT * FROM post ORDER BY postTime DESC LIMIT ' . $this_page_first_result . ',' . $threadLim;
-$result=mysqli_query($conn,$sql);
-
-while($row = mysqli_fetch_array($result)) {
-	
-	$postID = $row['postID'];
-	$cropID = $row['cropID'];
-	$query = "SELECT cropname FROM crop WHERE cropID=" . $cropID;
-	$cropname = mysqli_fetch_assoc(mysqli_query($conn, $query))['cropname'];
-	$title = $row['title'];
-	$posterID = $row['userID'];
-	$commentCount = $row['commentCount'];
-	$query = "SELECT username FROM user WHERE userID=" . $posterID;
-	$postUsername = mysqli_fetch_assoc(mysqli_query($conn, $query))['username'];
-	$content = $row['text'];
-	
-	echo '
-		<div class="w3-panel w3-border w3-round-small w3-padding-large" style="width:60%; background-color: white;" >
-			<a>in : <a href ="cropsubforum.php?cropID=' . $cropID . '">' . $cropname . '</a>
-			<h1><a href="threadview.php?postID=' . $postID . '">' . $title . '</a></h1>
-			<div style = "font-size : 13px;">
-				<a>by : <a href ="userprofile.php?userID=' . $posterID . '">' . $postUsername . '</a></a><br>
-				<a>' . $commentCount . ' comment(s)</a>
-			</div>
-			<p>' . $content . '</p>
-		</div>
-	';
-}
 
 //page number buttons
 $sql = "SELECT COUNT(postID) FROM post";//total number of threads
 $number_of_results = mysqli_fetch_assoc(mysqli_query($conn, $sql))['COUNT(postID)'];
 $number_of_pages = ceil($number_of_results/$threadLim);
 
-// Pagination
 $prev = $page - 1;
 $next = $page + 1;
 $currentPage = $page;
 
+//////////PAGE NUMBER BUTTONS DISPLAY START//////////
 echo '
-<div class="page">
+<div>
 ';
 
 //First and Previous buttons
@@ -148,6 +118,107 @@ if($currentPage <= $number_of_pages - 2){
 echo '
 </div>
 ';
+//////////PAGE NUMBER BUTTONS DISPLAY END//////////
+
+$this_page_first_result = ($currentPage-1)*$threadLim;
+
+$sql ='SELECT * FROM post ORDER BY postTime DESC LIMIT ' . $this_page_first_result . ',' . $threadLim;
+$result=mysqli_query($conn,$sql);
+
+while($row = mysqli_fetch_array($result)) {
+	
+	$postID = $row['postID'];
+	$cropID = $row['cropID'];
+	$query = "SELECT cropname FROM crop WHERE cropID=" . $cropID;
+	$cropname = mysqli_fetch_assoc(mysqli_query($conn, $query))['cropname'];
+	$title = $row['title'];
+	$posterID = $row['userID'];
+	$commentCount = $row['commentCount'];
+	$query = "SELECT username FROM user WHERE userID=" . $posterID;
+	$postUsername = mysqli_fetch_assoc(mysqli_query($conn, $query))['username'];
+	$content = $row['text'];
+	
+	echo '
+		<div class="w3-panel w3-border w3-round-small w3-padding-large" style="width:60%; background-color: white;" >
+			<a>in : <a href ="cropsubforum.php?cropID=' . $cropID . '">' . $cropname . '</a>
+			<h1><a href="threadview.php?postID=' . $postID . '">' . $title . '</a></h1>
+			<div style = "font-size : 13px;">
+				<a>by : <a href ="userprofile.php?userID=' . $posterID . '">' . $postUsername . '</a></a><br>
+				<a>' . $commentCount . ' comment(s)</a>
+			</div>
+			<p>' . $content . '</p>
+		</div>
+	';
+}
+
+//////////PAGE NUMBER BUTTONS DISPLAY START//////////
+echo '
+<div>
+';
+
+//First and Previous buttons
+if($currentPage>=3){
+	echo '<a href="homepage.php?page=' . '1' . '">' ."<button><<</button>" .'</a>';
+}
+if($currentPage > 1){
+	echo '<a href="homepage.php?page=' . $prev . '">' ."<button><</button>" .'</a>';
+}
+
+//Page number buttons	
+if($currentPage<=3){
+	for ($page=1;$page<=7;$page++) {
+		// if page==currentPage, change colour
+		if($page>=1){
+			if($page==$currentPage){
+				echo '<a href="homepage.php?page=' . $page . '">' . '<button style="background-color:#4CAF50">' . $page . '</button></a>';
+			}
+			else if ($page > 0 && $page <= $number_of_pages){
+				echo '<a href="homepage.php?page=' . $page . '">' . '<button>' . $page . '</button></a>';
+			}				
+		}
+	}	
+}
+else if($currentPage > $number_of_pages - 3){
+	for($page = $number_of_pages - 6; $page <= $number_of_pages; $page++){
+		// if page==currentPage, change colour
+		if($page>=1){
+			if($page==$currentPage){
+				echo '<a href="homepage.php?page=' . $page . '">' . '<button style="background-color:#4CAF50">' . $page . '</button></a>';
+			}
+			else if ($page > 0 && $page <= $number_of_pages){
+				echo '<a href="homepage.php?page=' . $page . '">' . '<button>' . $page . '</button></a>';
+			}				
+		}
+	}
+}
+else{	
+	for ($page=$currentPage-3;$page<=$currentPage+3;$page++) {
+		// if page==currentPage, change colour
+		if($page>=1){
+			if($page==$currentPage){
+				echo '<a href="homepage.php?page=' . $page . '">' . '<button style="background-color:#4CAF50">' . $page . '</button></a>';
+			}
+			else if ($page > 0 && $page <= $number_of_pages){
+				echo '<a href="homepage.php?page=' . $page . '">' . '<button>' . $page . '</button></a>';
+			}
+		
+		}
+	}
+}
+
+//Next and Last buttons
+if($currentPage < $number_of_pages){
+	echo '<a href="homepage.php?page=' . $next . '">' .'<button>></button></a>';
+}
+if($currentPage <= $number_of_pages - 2){
+	echo '<a href="homepage.php?page=' . $number_of_pages . '">' .'<button>>></button></a>';
+
+}
+
+echo '
+</div>
+';
+//////////PAGE NUMBER BUTTONS DISPLAY END//////////
 
 ?>
 </body>
--- userprofile.php
+++ userprofile.php
@@ -74,46 +74,20 @@ if(!isset($_GET['page'])){
 else{
 	$page = $_GET['page'];
 }
-$this_page_first_result = ($page-1)*$threadLim;
-
-$sql="SELECT * FROM post WHERE userID=" . $userID . " ORDER BY postTime DESC LIMIT " . $this_page_first_result . ',' . $threadLim;
-$result = mysqli_query($conn, $sql);
-
-while($row = mysqli_fetch_assoc($result)) {
-	
-	$postID = $row['postID'];
-	$cropID = $row['cropID'];
-	$query = "SELECT cropname FROM crop WHERE cropID=" . $cropID;
-	$cropname = mysqli_fetch_assoc(mysqli_query($conn, $query))['cropname'];
-	$title = $row['title'];
-	$posterID = $row['userID'];
-	$commentCount = $row['commentCount'];
-	$query = "SELECT username FROM user WHERE userID=" . $posterID;
-	$postUsername = mysqli_fetch_assoc(mysqli_query($conn, $query))['username'];
-	$content = $row['text'];
-	
-	echo '
-		<div class="w3-panel w3-border w3-round-small w3-padding-large" style="width:60%; background-color: white;" >
-			<a>in : <a href ="cropsubforum.php?cropID=' . $cropID . '">' . $cropname . '</a>
-			<h1><a href="threadview.php?postID=' . $postID . '">' . $title . '</a></h1>
-			<div style = "font-size : 13px;">
-				<a>by : <a href ="userprofile.php?userID=' . $posterID . '">' . $postUsername . '</a></a><br>
-				<a>' . $commentCount . ' comment(s)</a>
-			</div>
-			<p>' . $content . '</p>
-		</div>
-	';
-}
 
 //page number buttons
 $sql = "SELECT COUNT(postID) FROM post WHERE userID=" . $userID;//total number of threads
 $number_of_results = mysqli_fetch_assoc(mysqli_query($conn, $sql))['COUNT(postID)'];
 $number_of_pages = ceil($number_of_results/$threadLim);
 
-// Pagination
 $prev = $page - 1;
 $next = $page + 1;
 $currentPage = $page;
+
+//////////PAGE NUMBER BUTTONS DISPLAY START//////////
+echo '
+<div>
+';
 
 //First and Previous buttons
 if($currentPage>=3){
@@ -172,6 +146,113 @@ if($currentPage < $number_of_pages){
 if($currentPage <= $number_of_pages - 2){
 	echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $number_of_pages . '">' .'<button>>></button></a>';
 }
+
+
+echo '
+</div>
+';
+//////////PAGE NUMBER BUTTONS DISPLAY END//////////
+
+$this_page_first_result = ($currentPage-1)*$threadLim;
+
+$sql="SELECT * FROM post WHERE userID=" . $userID . " ORDER BY postTime DESC LIMIT " . $this_page_first_result . ',' . $threadLim;
+$result = mysqli_query($conn, $sql);
+
+while($row = mysqli_fetch_assoc($result)) {
+	
+	$postID = $row['postID'];
+	$cropID = $row['cropID'];
+	$query = "SELECT cropname FROM crop WHERE cropID=" . $cropID;
+	$cropname = mysqli_fetch_assoc(mysqli_query($conn, $query))['cropname'];
+	$title = $row['title'];
+	$posterID = $row['userID'];
+	$commentCount = $row['commentCount'];
+	$query = "SELECT username FROM user WHERE userID=" . $posterID;
+	$postUsername = mysqli_fetch_assoc(mysqli_query($conn, $query))['username'];
+	$content = $row['text'];
+	
+	echo '
+		<div class="w3-panel w3-border w3-round-small w3-padding-large" style="width:60%; background-color: white;" >
+			<a>in : <a href ="cropsubforum.php?cropID=' . $cropID . '">' . $cropname . '</a>
+			<h1><a href="threadview.php?postID=' . $postID . '">' . $title . '</a></h1>
+			<div style = "font-size : 13px;">
+				<a>by : <a href ="userprofile.php?userID=' . $posterID . '">' . $postUsername . '</a></a><br>
+				<a>' . $commentCount . ' comment(s)</a>
+			</div>
+			<p>' . $content . '</p>
+		</div>
+	';
+}
+
+//////////PAGE NUMBER BUTTONS DISPLAY START//////////
+echo '
+<div>
+';
+
+//First and Previous buttons
+if($currentPage>=3){
+	echo '<a href="userprofile.php?userID=' . $userID . '&page=' . '1' . '">' ."<button><<</button>" .'</a>';
+}
+if($currentPage > 1){
+	echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $prev . '">' ."<button><</button>" .'</a>';
+}
+
+//Page number buttons	
+if($currentPage<=3){
+	for ($page=1;$page<=7;$page++) {
+		// if page==currentPage, change colour
+		if($page>=1){
+			if($page==$currentPage){
+				echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $page . '">' . '<button style="background-color:#4CAF50">' . $page . '</button></a>';
+			}
+			else if ($page > 0 && $page <= $number_of_pages){
+				echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $page . '">' . '<button>' . $page . '</button></a>';
+			}				
+		}
+	}	
+}
+else if($currentPage > $number_of_pages - 3){
+	for($page = $number_of_pages - 6; $page <= $number_of_pages; $page++){
+		// if page==currentPage, change colour
+		if($page>=1){
+			if($page==$currentPage){
+				echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $page . '">' . '<button style="background-color:#4CAF50">' . $page . '</button></a>';
+			}
+			else if ($page > 0 && $page <= $number_of_pages){
+				echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $page . '">' . '<button>' . $page . '</button></a>';
+			}				
+		}
+	}
+}
+else{	
+	for ($page=$currentPage-3;$page<=$currentPage+3;$page++) {
+		// if page==currentPage, change colour
+		if($page>=1){
+			if($page==$currentPage){
+				echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $page . '">' . '<button style="background-color:#4CAF50">' . $page . '</button></a>';
+			}
+			else if ($page > 0 && $page <= $number_of_pages){
+				echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $page . '">' . '<button>' . $page . '</button></a>';
+			}
+		
+		}
+	}
+}
+
+//Next and Last buttons
+if($currentPage < $number_of_pages){
+	echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $next . '">' .'<button>></button></a>';
+}
+if($currentPage <= $number_of_pages - 2){
+	echo '<a href="userprofile.php?userID=' . $userID . '&page=' . $number_of_pages . '">' .'<button>>></button></a>';
+}
+
+
+echo '
+</div>
+';
+//////////PAGE NUMBER BUTTONS DISPLAY END//////////
+
 ?>	
 
 </body>
